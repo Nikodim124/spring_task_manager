26627216aec7890fac6296e9e17f7d69295005c4
Nikodim124 <52649614+Nikodim124@users.noreply.github.com>
2022-10-05 14:19:25 +0500

    Add files via upload

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -26,7 +26,38 @@ function init() {
         myMap.geoObjects.add(placemarker);
     }
 
-   
+    /**ymaps.geocode('Пермь, Челюскинцев, 23', {
+        results: 1
+    }).then(function (res) {
+        var firstGeoObject = res.geoObjects.get(0),
+            coords = firstGeoObject.geometry.getCoordinates(),
+            bounds = firstGeoObject.properties.get('boundedBy');
+
+        firstGeoObject.options.set('preset', 'islands#darkBlueDotIconWithCaption');
+        firstGeoObject.properties.set('iconCaption', firstGeoObject.getAddressLine());
+
+        myMap.geoObjects.add(firstGeoObject);
+        myMap.setBounds(bounds, {
+            checkZoomRange: true
+        });
+
+        console.log('Все данные геообъекта: ', firstGeoObject.properties.getAll());
+
+        console.log('Метаданные ответа геокодера: ', res.metaData);
+        console.log('Метаданные геокодера: ', firstGeoObject.properties.get('metaDataProperty.GeocoderMetaData'));
+
+        console.log('precision', firstGeoObject.properties.get('metaDataProperty.GeocoderMetaData.precision'));
+
+        console.log('Тип геообъекта: %s', firstGeoObject.properties.get('metaDataProperty.GeocoderMetaData.kind'));
+        console.log('Название объекта: %s', firstGeoObject.properties.get('name'));
+        console.log('Описание объекта: %s', firstGeoObject.properties.get('description'));
+        console.log('Полное описание объекта: %s', firstGeoObject.properties.get('text'));
+        console.log('\nГосударство: %s', firstGeoObject.getCountry());
+        console.log('Населенный пункт: %s', firstGeoObject.getLocalities().join(', '));
+        console.log('Адрес объекта: %s', firstGeoObject.getAddressLine());
+        console.log('Наименование здания: %s', firstGeoObject.getPremise() || '-');
+        console.log('Номер здания: %s', firstGeoObject.getPremiseNumber() || '-');
+    });*/
 }
 
 function getCoords() {
